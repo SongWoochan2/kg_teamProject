463b202a4936844db85815d35196e69b98824748
SongWoochan <ohmygoddu@naver.com>
2018-04-13 16:04:51 +0900

    등록

---
+++ src/main/java/showPlace/bean/ShowPlaceVO.java
@@ -0,0 +1,47 @@
+package showPlace.bean;
+
+public class ShowPlaceVO {
+	private int show_place_code;
+	private int theater_code;
+	private String show_place_name;
+	private int seat_num;
+	private int default_cost;
+	private String show_place_photo_addr;
+	public int getShow_place_code() {
+		return show_place_code;
+	}
+	public void setShow_place_code(int show_place_code) {
+		this.show_place_code = show_place_code;
+	}
+	public int getTheater_code() {
+		return theater_code;
+	}
+	public void setTheater_code(int theater_code) {
+		this.theater_code = theater_code;
+	}
+	public String getShow_place_name() {
+		return show_place_name;
+	}
+	public void setShow_place_name(String show_place_name) {
+		this.show_place_name = show_place_name;
+	}
+	public int getSeat_num() {
+		return seat_num;
+	}
+	public void setSeat_num(int seat_num) {
+		this.seat_num = seat_num;
+	}
+	public int getDefault_cost() {
+		return default_cost;
+	}
+	public void setDefault_cost(int default_cost) {
+		this.default_cost = default_cost;
+	}
+	public String getShow_place_photo_addr() {
+		return show_place_photo_addr;
+	}
+	public void setShow_place_photo_addr(String show_place_photo_addr) {
+		this.show_place_photo_addr = show_place_photo_addr;
+	}
+
+}
