bb91e7a20a5dfc973269960bb86c82aa3bdef0df
jinhwan <jinhwanea@naver.com>
2018-04-25 21:47:11 +0900

    answer 추가중

---
+++ src/main/java/answer/controller/AnswerService.java
@@ -0,0 +1,30 @@
+package answer.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import answer.bean.AnswerDTO;
+import answer.dao.AnswerDAO;
+
+
+@Service
+public class AnswerService {
+	
+	/*@Autowired
+	private AnswerDAO answerDAO;
+	
+	public List<AnswerDTO> answerList(int startNum, int endNum){
+		return answerDAO.answerList(startNum, endNum);
+	}
+	
+	public AnswerDTO answerView(int answer_code) {
+		return answerDAO.answerView(answer_code);
+	}
+	
+	public int getTotalA() {
+		return answerDAO.getTotalA();
+	}*/
+	
+}
